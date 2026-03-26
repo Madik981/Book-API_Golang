c10004aeab2763faaa4b35b1e7ed809ea9f860d4
Madik981 <madi12312456@gmail.com>
2026-03-26 10:05:02 +0500

    Connected codes with main

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/gorilla/mux"
+
+	"Book-API_Golang/handlers"
+	"Book-API_Golang/models"
+)
+
+func main() {
+	store := models.NewStore()
+
+	r := mux.NewRouter()
+
+	bookHandler := &handlers.BookHandler{Store: store}
+	authorHandler := &handlers.AuthorHandler{Store: store}
+	categoryHandler := &handlers.CategoryHandler{Store: store}
+
+	bookHandler.RegisterRoutes(r)
+	authorHandler.RegisterRoutes(r)
+	categoryHandler.RegisterRoutes(r)
+
+	log.Println("server started on :8080")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal(err)
+	}
 }
